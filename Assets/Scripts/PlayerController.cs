1e2f4a9c80285636b13a012bf68c3abf42871dac
William Gimenes <willsgimenes@gmail.com>
2020-04-28 15:42:04 -0300

    refactoring player controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,86 +7,34 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float jumpForce;
     private float _moveInput;
-
     private Rigidbody2D _rb;
-
     private bool _facingRight = true;
-
-
     public bool isGrounded;
-
     public Transform groundCheck;
-    
     public float checkRadius;
-
     public LayerMask whatIsGround;
-
     private int _extraJumps;
-
     public int extraJumpsValue;
-
     public bool isJumping;
-
     private float _jumpTimeCounter;
-
     public float JumpTime;
-
 
 
     // Start is called before the first frame update
     void Start()
     {
         _extraJumps = extraJumpsValue;
-
         _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (isGrounded == true)
-        {
-            _extraJumps = extraJumpsValue;
-        }
-
-        // single jump
-        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space) && _extraJumps == 0)
-        {
-            isJumping = true;
-            _jumpTimeCounter = JumpTime;
-            _rb.velocity = Vector2.up * jumpForce;
-        }
-
-        if (Input.GetKey(KeyCode.Space) && isJumping == true)
-        {
-            if (_jumpTimeCounter > 0)
-            {
-                _rb.velocity = Vector2.up * jumpForce;
-                _jumpTimeCounter -= Time.deltaTime;
-            } else {
-                isJumping = false;
-            }
-        }
-
-        if (Input.GetKeyUp(KeyCode.Space))
-        {    
-            isJumping = false;
-        }
-
-
-        if (Input.GetKeyDown(KeyCode.Space) && _extraJumps >0)
-        {
-            isJumping = true;
-            _jumpTimeCounter = JumpTime;
-            _rb.velocity = Vector2.up * jumpForce / 3;
-            _extraJumps--;
-        }
+        Jump();
     }
 
     private void FixedUpdate()
     {
-
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
 
         _moveInput = Input.GetAxis("Horizontal");
@@ -99,14 +47,58 @@ public class PlayerController : MonoBehaviour
         {
             Flip();
         }
-
     }
 
     private void Flip()
     {
         _facingRight = !_facingRight;
-        Vector3 scaler = transform.localScale;
+        var transform1 = transform;
+        Vector3 scaler = transform1.localScale;
         scaler.x *= -1;
-        transform.localScale = scaler;
+        transform1.localScale = scaler;
+    }
+
+    private void Jump()
+    {
+        if (isGrounded)
+        {
+            _extraJumps = extraJumpsValue;
+        }
+
+        // TODO: remove this Input Input.GetKeyDown 
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space) && _extraJumps == 0)
+        {
+            isJumping = true;
+            _jumpTimeCounter = JumpTime;
+            _rb.velocity = Vector2.up * jumpForce;
+        }
+
+        // TODO: remove this Input Input.GetKeyDown
+        if (Input.GetKey(KeyCode.Space) && isJumping)
+        {
+            if (_jumpTimeCounter > 0)
+            {
+                _rb.velocity = Vector2.up * jumpForce;
+                _jumpTimeCounter -= Time.deltaTime;
+            } else {
+                isJumping = false;
+            }
+        }
+
+        // TODO: remove this Input Input.GetKeyDown
+        if (Input.GetKeyUp(KeyCode.Space))
+        {    
+            isJumping = false;
+        }
+
+
+        // TODO: remove this Input Input.GetKeyDown
+        if (Input.GetKeyDown(KeyCode.Space) && _extraJumps > 0)
+        {
+            isJumping = true;
+            _jumpTimeCounter = JumpTime;
+            _rb.velocity = Vector2.up * jumpForce / 3;
+            _extraJumps--;
+        }
     }
 }
